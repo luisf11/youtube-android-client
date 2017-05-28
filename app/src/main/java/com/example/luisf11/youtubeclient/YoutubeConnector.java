50fbe2794d4dd1e5539e3c2dffdd01501cfe86be
luisf11 <luisfernandop1110@gmail.com>
2017-05-28 11:45:46 -0400

    layout created

---
+++ app/src/main/java/com/example/luisf11/youtubeclient/YoutubeConnector.java
@@ -0,0 +1,13 @@
+package com.example.luisf11.youtubeclient;
+
+/**
+ * Created by luisf11 on 28/05/17.
+ */
+
+public class YoutubeConnector {
+
+    private Youtube youtube;
+    private Youtube.Search.List query;
+
+    public static final String KEY = "";
+}
