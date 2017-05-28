6a3c86fe58d7fc6c151642016b308dc9dcedf4b3
luisf11 <luisfernandop1110@gmail.com>
2017-05-28 15:39:06 -0400

    youtube connector added

--- app/src/main/java/com/example/luisf11/youtubeclient/YoutubeConnector.java
+++ app/src/main/java/com/example/luisf11/youtubeclient/YoutubeConnector.java
@@ -1,13 +1,69 @@
 package com.example.luisf11.youtubeclient;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+
+import java.io.IOException;
+
 /**
  * Created by luisf11 on 28/05/17.
  */
 
 public class YoutubeConnector {
 
-    private Youtube youtube;
-    private Youtube.Search.List query;
+    private YouTube youTube;
+    private YouTube.Search.List query;
 
-    public static final String KEY = "";
+    public static final  String KEY = "AIzaSyCGTKqQDTW4Zqz_1_SPcY_XNgrod-YIr_M";
+
+    public YoutubeConnector(Context context){
+        youTube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest httpRequest) throws IOException {
+
+            }
+        }).setApplicationName(context.getString(R.string.app_name)).build();
+
+        try {
+            query = youTube.search().list("id.snippet");
+            query.setKey(KEY);
+            query.setType("video");
+            query.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
+        }catch (IOException e){
+            Log.d("YC", "Could not initialize: "+ e.getMessage());
+        }
+    }
+    public List<VideoItem> search(String keywords){
+        query.setQ(keywords);
+        try {
+            SearchListResponse response = query.execute();
+            List<SearchResult> results = response.getItems();
+            List<VideoItem> items = new ArrayList<VideoItem>();
+
+            for(SearchResult result : results){
+                VideoItem item = new VideoItem();
+                item.setTitle(result.getSnippet().getTitle());
+                item.setDescription(result.getSnippet().getDescription());
+                item.setThumbnailURL(result.getSnippet().getThumbnails().getDefault().getUrl());
+                item.setId(result.getId().getVideoId());
+                items.add(item);
+            }
+            return items;
+        }catch (IOException e){
+            Log.d("YC", "Could not search: "+ e);
+            return null;
+        }
+
+
+    }
 }
