80069c2f5e0cdb073e976b6c2c6034916d99db1d
luisf11 <luisfernandop1110@gmail.com>
2017-06-13 05:49:31 -0400

    mouse scroll event in player activity

--- app/src/main/java/com/example/luisf11/youtubeclient/view/PlayerActivity.java
+++ app/src/main/java/com/example/luisf11/youtubeclient/view/PlayerActivity.java
@@ -5,6 +5,7 @@ package com.example.luisf11.youtubeclient.view;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
@@ -41,6 +42,25 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
         Toast.makeText(this,"Initialization Failed",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        Log.i("hey listen","hey Listen");
+        int AXIS_VSCROLL =0;
+//        event.getAxisValue(AXIS_VSCROLL);
+        if ( event.getAxisValue(MotionEvent.AXIS_VSCROLL)==1){
+            audio.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_RAISE,AudioManager.FLAG_SHOW_UI);
+            return true;
+        }else if( event.getAxisValue(MotionEvent.AXIS_VSCROLL)==-1){
+            audio.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_LOWER,AudioManager.FLAG_SHOW_UI);
+            return true;
+        }
+      //  return  false;
+
+        return super.onGenericMotionEvent(event);
     }
 
     @Override
